cb29016645687ec7e8b76dd41e3e7e204e08620c
sailaja <bsailaja4398@gmail.com>
2022-01-11 14:37:52 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amreactauthd51a8fa3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import Amplify, { Auth, Hub } from "aws-amplify";
+
+
+const initialFormState = {
+  username: "",
+  password: "",
+  email: "",
+  authCode: "",
+  formType: "signIn",
+};
 
 function App() {
+  const [formState, updateFormState] = useState(initialFormState);
+
+  const [user, updateUser] = useState(null);
+
+  const checkUser = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+
+      updateUser(user);
+
+      console.log("got user", user);
+
+      updateFormState(() => ({ ...formState, formType: "signedIn" }));
+    } catch (err) {
+      console.log("checkUser error", err);
+      updateFormState(() => ({ ...formState, formType: "signIn" }));
+    }
+  };
+
+  // Skip this if you're not using Hub. You can call updateFormState function right
+  // after the Auth.signOut() call in the button.
+  const setAuthListener = async () => {
+    Hub.listen("auth", (data) => {
+      switch (data.payload.event) {
+        case "signOut":
+          console.log(data);
+
+          updateFormState(() => ({
+            ...formState,
+            formType: "signIn",
+          }));
+
+          break;
+        case "signIn":
+          console.log(data);
+
+          break;
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkUser();
+    setAuthListener();
+  }, []);
+
+  const onChange = (e) => {
+    e.persist();
+    updateFormState(() => ({ ...formState, [e.target.name]: e.target.value }));
+  };
+
+  const { formType } = formState;
+
+  const signUp = async () => {
+    const { username, email, password } = formState;
+
+    await Auth.signUp({ username, password, attributes: { email } });
+
+    updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
+  };
+
+  const confirmSignUp = async () => {
+    const { username, authCode } = formState;
+
+    await Auth.confirmSignUp(username, authCode);
+
+    updateFormState(() => ({ ...formState, formType: "signIn" }));
+  };
+
+  const signIn = async () => {
+    const { username, password } = formState;
+
+    await Auth.signIn(username, password);
+
+    updateFormState(() => ({ ...formState, formType: "signedIn" }));
+  };
+
+  console.log(formState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>App</h1>
+
+      {formType === "signUp" && (
+        <div>
+          <input name="username" onChange={onChange} placeholder="username" />
+          <input
+            name="password"
+            type="password"
+            onChange={onChange}
+            placeholder="password"
+          />
+          <input name="email" onChange={onChange} placeholder="email" />
+
+          <button onClick={signUp}>Sign Up</button>
+
+          <p>Already signed up?</p>
+
+          <button
+            onClick={() =>
+              updateFormState(() => ({
+                ...formState,
+                formType: "signIn",
+              }))
+            }
+          >
+            Sign In instead
+          </button>
+        </div>
+      )}
+
+      {formType === "confirmSignUp" && (
+        <div>
+          <input
+            name="authCode"
+            onChange={onChange}
+            placeholder="cnfirm auth code"
+          />
+          <button onClick={confirmSignUp}>Confirm Sign up</button>
+        </div>
+      )}
+
+      {formType === "signIn" && (
+        <div>
+          <input name="username" onChange={onChange} placeholder="username" />
+          <input
+            name="password"
+            type="password"
+            onChange={onChange}
+            placeholder="password"
+          />
+          <button onClick={signIn}>Sign In</button>
+
+          <p>No account yet?</p>
+
+          <button
+            onClick={() =>
+              updateFormState(() => ({
+                ...formState,
+                formType: "signUp",
+              }))
+            }
+          >
+            Sign Up now
+          </button>
+        </div>
+      )}
+
+      {formType === "signedIn" && (
+        <div>
+          <h2>
+            Welcome the app, {user.username} ({user.attributes.email})!
+          </h2>
+
+          <button
+            onClick={() => {
+              Auth.signOut();
+            }}
+          >
+            Sign out
+          </button>
+        </div>
+      )}
+
+      <hr />
+    </>
   );
 }
 
